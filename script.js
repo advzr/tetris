cf4b94375467fbf15fd45d2653b01ebc05f88471
Dmitry Gavrikov <dmitry923@gmail.com>
2013-06-20 20:30:51 +0700

    Add script structure with some descriptions

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/* state object
+ * state.pieces - all pieces that are already on the bottom
+ * of the field and are not moving
+ * state.occupiedField - a field with sections marked as
+ * occipied by state.pieces
+ * state.currentPiece - the current active piece controlled
+ * by the "key module"
+ * state.nextPiece - the next piece that will come after state.currentPiece
+ * state.lines - a quantity of cleared lines
+ * state.level - a difficulty level. The higher the level the more
+ * frequently the timeTick function is called */
+
+/* 1) module key
+ * up(). down(), left(). right()
+ * Catches keyboard events and calls the move module */
+
+/* 2) module move
+ * up(), down(), left(), right()
+ * Moves the last piece in the field by generating newCoords, then checks
+ * newCoords with the check function. If the check function returns true,
+ * replaces currentCoords with newCoords and calls the drawGame function. */
+
+/* 3) function check(coords)
+ * Returns true if coords are within field and are not occupied by
+ * other pieces */
+
+/* 4) timeTick function
+ * Tries to call move.down(). If fails then pushes it into state.pieces,
+ * updates state.occupiedField, calls the clearLine function and calls
+ * the createRandomPiece function */
+
+/* 5) createRandomPiece function
+ * Creates new random piece, puts it in window as a next piece,
+ * takes the next piece out of that window and puts it in the array of pieces */
+
+/* 6) drawGame function
+ * Draws all pieces on the field
+ * there are subfunctions:
+ *  to draw state.pieces
+ *  to draw state.currentPiece
+ *  to draw state.nextPiece
+ *  to animate a cleared line */
+
+/* 7) clearLine function
+ * Removes a full line consisting of state.pieces and unshifts their coordinates
+ * filling them with an empty line */
